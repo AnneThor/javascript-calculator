40401026c8ca8a820c8feb1db997d9f0a73d4de1
Anne Thor <anne.thor@gmail.com>
2020-06-23 21:19:31 -0700

    fix #1 created github page and started basic layout

--- src/App.js
+++ src/App.js
@@ -4,9 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
 
+      <header className="App-header">
+        <ul className="header-nav-bar">
+          <li className="project-title"><a href="https://annethor.github.io/javascript-calculator">Javascript Calculator</a></li>
+          <li className="home"><a href="https://annethor.github.io" target="_">Back to Portfolio</a></li>
+          <li className="author">Designed & Coded by Anne Thorsteinson</li>
+        </ul>
       </header>
+
+      <div id="calculator-display">
+
+        <section id="display">
+          Here is where we will put the inputs
+        </section>
+
+        <section id="calculator-buttons">
+          Here is where we will put buttons
+        </section>
+
+      </div>
+
+
     </div>
   );
 }
