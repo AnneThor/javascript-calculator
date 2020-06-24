3cdc33f9f032b174e999a53d9f1677a0eec3f863
Anne Thor <anne.thor@gmail.com>
2020-06-23 20:42:26 -0700

    Updates

---
+++ precache-manifest.f251c5ba0cd3eb4d239f0bc9474bfb99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7a4a37b1f1e84433de0c9f1f511087",
+    "url": "/javascript-calculator/index.html"
+  },
+  {
+    "revision": "f35c39865801c8347a9e",
+    "url": "/javascript-calculator/static/css/main.7c421f3c.chunk.css"
+  },
+  {
+    "revision": "8b573998b821b59d43d7",
+    "url": "/javascript-calculator/static/js/2.b7f5363d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/javascript-calculator/static/js/2.b7f5363d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35c39865801c8347a9e",
+    "url": "/javascript-calculator/static/js/main.1693bf82.chunk.js"
+  },
+  {
+    "revision": "2a1d30e85db36d30dcbf",
+    "url": "/javascript-calculator/static/js/runtime-main.f3cb5538.js"
+  }
+]);
